1b2245c6929391a1bd5f6d8f50bb9e3ad5ae591c
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-11-12 13:10:07 +0530

    Add files via upload

---
+++ jsDay2.js
@@ -0,0 +1,123 @@
+console.log("hello World");
+
+
+// let a;
+// console.log(typeof(a));
+
+// Voting  Machine 
+
+// let age = 120; 
+
+// if(age >= 18 && age < 120) {
+// console.log("Eligible for Voting...! ")
+// }else{
+//   console.log("NOT Eligible for Voting...! ")  
+// }
+
+
+4
+100
+400
+
+
+// let yr = 2020;
+
+
+// if(yr %4 ==0 || yr %100 ==0 || yr %400 ==0){
+// console.log("This is LEAP YR... !");
+// }else{
+//    console.log("This is NOT LEAP YR... !"); 
+// }
+
+
+
+
+// Days 
+
+    
+// let day = "tuesday"
+
+//    switch(day) {
+ 
+//     case "monday" : 
+//     console.log("Monday .. !"); 
+//     break
+//     case "tuesday" : 
+//     console.log("tuesday .. !"); 
+//     break
+//     case "Wednesday" : 
+//     console.log("Wednesday .. !"); 
+//     break;
+//     case "Thusday" : 
+//     console.log("Thusday .. !"); 
+//     break;
+//     case "Friday" : 
+//     console.log("Friday .. !"); 
+//     break;
+//     case "Saturday" : 
+//     console.log("Saturday .. !"); 
+//     break;
+
+//     default :
+//     console.log("Sunday .. !")    
+
+
+
+// }
+
+
+// for(let i = 1; i<=100;i++){
+//     if(i%2 ==0)
+// console.log("EVEN Num : " +i);
+//     else{
+//       console.log("Odd Num : "+i);  
+//     }
+// } 
+
+
+
+// let i = 1;
+
+// while(i<=100){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i = 1
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=100)
+
+
+//   Array :
+
+// let arr = [1,2,6,3,6,3,6,2,7,3,7,3];
+// console.log(arr.length);
+// console.log(arr.push(34));
+// console.log(arr);
+// arr.unshift(22);
+// console.log();
+
+// console.log(arr);
+// arr.revers;
+// console.log(arr);
+
+
+// String 
+
+
+let s = "The Kiran Academy...! ";
+
+console.log(s.length);
+console.log(s.toLowerCase());
+console.log(s.toUpperCase());
+
+console.log(s.indexOf("K"));
+
+
+
+
+
