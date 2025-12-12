b1df70830dad64c1092232f5f1ee1d4b0a8e92e1
ShubhamBarse2 <shubhambarse510@gmail.com>
2025-12-12 12:31:10 +0530

    Add files via upload

---
+++ SpringBootDay5/src/main/java/com/jbk/service/EmployeeService.java
@@ -0,0 +1,46 @@
+package com.jbk.service;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jbk.dao.EmployeeDao;
+import com.jbk.entities.Employee;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	EmployeeDao dao;
+
+	public String insertData(Employee e) {
+		String msg = dao.insertData(e);
+		if (Objects.isNull(msg)) {
+			msg = "Data is not Inserted...!";
+		}
+		return msg;
+	}
+
+	public String deleteData(int id) {
+		String msg = dao.deleteData(id);
+		return msg;
+	}
+
+	public String updateData(Employee e, int id) {
+		String msg = dao.updateData(e, id);
+		return msg;
+	}
+
+	public Employee getSingleData(int id) {
+		Employee e = dao.getSingleData(id);
+		return e;
+	}
+
+	public List<Employee> getAllData() {
+		List<Employee> list = dao.getAllData();
+		return list;
+	}
+
+}
